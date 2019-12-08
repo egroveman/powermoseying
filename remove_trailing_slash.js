4cf8bf2a02d5bddf3ce30146ba9424be8b1451c9
Ethan <ethan.groveman@gmail.com>
2019-12-08 09:24:26 -0500

    Create remove_trailing_slash.js

---
+++ remove_trailing_slash.js
@@ -0,0 +1,8 @@
+function stripTrailingSlash(str) {
+    if(str.substr(-1) === '/') {
+        return str.substr(0, str.length - 1);
+    }
+    return str;
+}
+
+window.location.href = stripTrailingSlash(window.location.href);
